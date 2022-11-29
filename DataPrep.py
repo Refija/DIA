56161ae5cc8ff5115113fe7496a21a2ff9bd3e19
Günther Moser <moser@tugraz.at>
2022-11-29 16:43:28 +0100

    split dataframes up

--- DataPrep.py
+++ DataPrep.py
@@ -17,20 +17,30 @@ dblp2data = pd.read_csv('Data/DBLP2.csv', encoding = "ISO-8859-1")
 
 
 ### Part 1 Prepare data
-# Merge both files into one
-dataframe = pd.concat([acmdata, dblp2data])
-print(dataframe)
+# Merge both files into one.
+# Have a look at https://pandas.pydata.org/docs/user_guide/merging.html
+#dataframe = pd.concat([acmdata, dblp2data])
+#print(dataframe)
 
 # Check if values are missing.
 # Have a look at https://towardsdatascience.com/data-cleaning-with-python-and-pandas-detecting-missing-values-3e9c6ebcf78b
-dataframe.dropna(
+acmdata.dropna(
     axis=0,
     how='any', # all = row completely empty, any = a single cell empty
     subset=None,
     inplace=True
 )
-print("Missing values")
-print(dataframe)
+#print("Missing values")
+#print(acmdata)
+dblp2data.dropna(
+    axis=0,
+    how='any', # all = row completely empty, any = a single cell empty
+    subset=None,
+    inplace=True
+)
+#print("Missing values")
+#print(acmdata)
+
 
 # Replace umlaut charaters 
 # Have a look at https://towardsdatascience.com/data-processing-example-using-python-bfbe6f713d9c , 
@@ -43,15 +53,21 @@ dictionary = {'&#196;': 'Ä', '&#228;': 'ä', '&#203;': 'Ë', '&#235;': 'ë', '&
               '&#231;': 'ç', '&#200;': 'È', '&#232;': 'è', '&#201;': 'É', '&#234;': 'ê', '&#209;': 'Ñ',
               '&#241;': 'ñ', '&#210;': 'Ò', '&#242;': 'ò', '&#211;': 'Ó', '&#243;': 'ó', '&#212;': 'Ô',
               '&#244;': 'ô', '&#245;': 'õ', '&#195;': 'Ÿ', '&#255;': 'ÿ', '&mdash;': '—'}
-dataframe.replace(dictionary, regex=True, inplace=True)
-print("replace umlaut")
-print(dataframe)
+acmdata.replace(dictionary, regex=True, inplace=True)
+#print("replace umlaut")
+#print(dataframe)
+dblp2data.replace(dictionary, regex=True, inplace=True)
+#print("replace umlaut")
+#print(dataframe)
 
 # Check for duplicates
 # Have a look at https://thispointer.com/python-3-ways-to-check-if-there-are-duplicates-in-a-list/
-dataframe = dataframe.drop_duplicates()
+acmdata = acmdata.drop_duplicates()
 print('drop dublicate')
-print(dataframe)
+print(acmdata)
+dblp2data = dblp2data.drop_duplicates()
+print('drop dublicate')
+print(dblp2data)
 
 
 ### Part 2 Implement a blocking scheme
