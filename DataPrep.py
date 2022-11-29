a5c818dd513bd6be46e19c776e5e8b57a5ab4901
Günther Moser <moser@tugraz.at>
2022-11-29 16:24:48 +0100

    first changes

--- DataPrep.py
+++ DataPrep.py
@@ -1,28 +1,61 @@
+
 import pandas as pd
 import numpy as np
 
 acmdata = pd.read_csv('Data/ACM.csv')
-print(acmdata)
+#print(acmdata)
 
 dblp2data = pd.read_csv('Data/DBLP2.csv', encoding = "ISO-8859-1")
-print(dblp2data)
+#print(dblp2data)
 
+### Part 0 Useful links
 # https://textblob.readthedocs.io/en/dev/
 # https://stanfordnlp.github.io/CoreNLP/
 # https://github.com/RaRe-Technologies/gensim
 # 
 
+
+
+### Part 1 Prepare data
+# Merge both files into one
+#dataframe = pd.concat([acmdata, dblp2data])
+#print(dataframe)
+
 # Check if values are missing.
 # Have a look at https://towardsdatascience.com/data-cleaning-with-python-and-pandas-detecting-missing-values-3e9c6ebcf78b
+acmdata.dropna(
+    axis=0,
+    how='any', # all = row completely empty, any = a single cell empty
+    subset=None,
+    inplace=True
+)
+#print(acmdata)
 
-# Check for "bad" charaters 
-# Have a look at https://towardsdatascience.com/data-processing-example-using-python-bfbe6f713d9c
+# Replace umlaut charaters 
+# Have a look at https://towardsdatascience.com/data-processing-example-using-python-bfbe6f713d9c , 
+# https://www.python-lernen.de/string-replace.htm and
+# https://www.designerinaction.de/tipps-tricks/web-development/html-umlaute-sonderzeichen/
 
-# Check for abbreviations
-# Have a look at 
+dictionary = {'&#196;': 'Ä', '&#228;': 'ä', '&#203;': 'Ë', '&#235;': 'ë', '&#207;': 'Ï', '&#239;': 'ï',
+              '&#214;': 'Ö', '&#246;': 'ö', '&#220;': 'Ü', '&#252;': 'ü', '&#223;': 'ß', '&#192;': 'À',
+              '&#224;': 'à', '&#193;': 'Á', '&#225;': 'á', '&#194;': 'Â', '&#226;': 'â', '&#199;': 'Ç',
+              '&#231;': 'ç', '&#200;': 'È', '&#232;': 'è', '&#201;': 'É', '&#234;': 'ê', '&#209;': 'Ñ',
+              '&#241;': 'ñ', '&#210;': 'Ò', '&#242;': 'ò', '&#211;': 'Ó', '&#243;': 'ó', '&#212;': 'Ô',
+              '&#244;': 'ô', '&#245;': 'õ', '&#195;': 'Ÿ', '&#255;': 'ÿ', '&mdash;': '—'}
+acmdata.replace(dictionary, regex=True, inplace=True)
+print(acmdata)
+
+# Check for duplicates
+# Have a look at https://thispointer.com/python-3-ways-to-check-if-there-are-duplicates-in-a-list/
+#print(acmdata.drop_duplicates())
+
+
+### Part 2 Implement a blocking scheme
+
+# Check for abbreviations and similar venue
+# Have a look at
+
 
 # Check for the "order" eg venue switched
 # Have a look at 
 
-# Check vor duplicates
-# Have a look at https://thispointer.com/python-3-ways-to-check-if-there-are-duplicates-in-a-list/
